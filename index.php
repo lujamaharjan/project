424a2d7d8106112e17d42cd1ec2fe15d746f6fcc
lujamaharjan <34180465+lujamaharjan@users.noreply.github.com>
2017-12-02 12:29:46 +0545

    upload index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <title>FoodNepal</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet"/>
+	
+	
+	<!--scripts-->
+	<script src="css/jquery-3.2.1.min.js"></script>
+	<script src="css/bootstrap.min.js"></script>
+
+	
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="images/spoon.ico">
+
+	
+ 
+ 
+  </head><!-- close head -->
+  <body>
+  
+  <header>
+  <?php include("header.php") ?>
+
+	
+  </header>
+  
+  
+ <div id="content-page"> <?php include("content-pages/index-content.php") ?></div><!-- content-page -->
+  
+  
+	
+	
+	
+	<div id="contact-us"> <?php include("contact-us.php") ?></div><!--contact-us-->	
+		
+	<footer>
+		<?php include("footer.php") ?>
+	</footer>
+	
+	
+	
+		
+	
+  </body>
+  </html>
